4787f6eeb0aa6901489784d3e0c504e8ce3ce295
razamehdisyed <razamehdisyed@gmail.com>
2023-05-23 21:19:45 +0500

    first

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -2,7 +2,7 @@ const Product = require('../models/product')
 const ErrorHandler = require('../utils/errorHandler')
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
 const APIFeatures = require('../utils/apifeatures')
-
+const CustomHttpError = require('../utils/errorHandler')
 // Create new product => /api/v1/admin/product/new
 
 exports.newProduct = catchAsyncErrors(async(req, res, next) => {
