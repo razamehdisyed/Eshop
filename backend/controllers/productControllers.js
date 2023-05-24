fc54afeabd551541581963bc472c96be00502545
razamehdisyed <razamehdisyed@gmail.com>
2023-05-24 21:01:39 +0500

    a

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -19,6 +19,7 @@ exports.getProducts = catchAsyncErrors(async (req, res, next)=> {
 
     const apifeatures = new APIFeatures(Product.find(),req.query)
                         .search()
+                        .filter()
 
     const products = await apifeatures.query
 
--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -15,5 +15,20 @@ class APIFeatures {
         this.query = this.query.find({ ...keyword })
         return this
     }
+
+    filter() {
+        const queryCopy = { ...this.queryStr }
+
+        //removing fields from query
+        const removeFields = ['keyword', 'limit', 'page']
+        removeFields.forEach(el => delete queryCopy[el])
+
+        //advance filter for price, rating etc
+        let queryStr = JSON.stringify(queryCopy)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+
+        this.query = this.query.find(JSON.parse(queryStr))
+        return this
+    }
 }
 module.exports = APIFeatures
