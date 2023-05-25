d041f290c2d7f9f1fccd3743733c6466919f1b0a
razamehdisyed <razamehdisyed@gmail.com>
2023-05-25 11:23:38 +0500

    user model

---
+++ backend/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter your name'],
+        maxLength : [30,'Your name cannot exceed 30 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter your email address'],
+        unique: true,
+        validate: [validator.isEmail, 'Please enter correct email address']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter your password'],
+        minLength: [6, 'Your password cannot exceed 30 characters'],
+        select: false
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            require: true
+        }
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date
+
+})
