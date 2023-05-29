063d2b1c597ca4900259000b626e898e6e2127a7
razamehdisyed <razamehdisyed@gmail.com>
2023-05-29 10:22:36 +0500

    s

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,7 +9,8 @@ const { registerUser,
         getUserProfile, 
         updatePassword, 
         updateProfile, 
-        allUsers} = require('../controllers/authController')
+        allUsers,
+        getUserDetails} = require('../controllers/authController')
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 
@@ -26,6 +27,7 @@ router.route('/me/update').put(isAuthenticatedUser, updateProfile)
 router.route('/password/update').put(isAuthenticatedUser, updatePassword)
 
 router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers)
+router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
 
 
 module.exports = router
