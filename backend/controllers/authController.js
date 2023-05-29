4f324c01335fcf39e1dcb85e5ccae311e0824486
razamehdisyed <razamehdisyed@gmail.com>
2023-05-29 09:59:41 +0500

    update profile

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -143,6 +143,26 @@ exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
     sendToken(user, 200, res)
 })
 
+//update user profile => api/v1/me/update
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email
+    }
+
+    // TO DO Avatar
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
+
 //logout user  => api/v1/logout
 
 exports.logout = catchAsyncErrors(async (req, res, next) => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserProfile, updatePassword } = require('../controllers/authController')
+const { registerUser,
+        loginUser, 
+        logout, 
+        forgotPassword, 
+        resetPassword, 
+        getUserProfile, 
+        updatePassword, 
+        updateProfile } = require('../controllers/authController')
+        
 const { isAuthenticatedUser } = require('../middlewares/auth')
 
 router.route('/register').post(registerUser)
@@ -10,6 +18,7 @@ router.route('/login').post(loginUser)
 router.route('/password/forgot').post(forgotPassword)
 router.route('/password/reset/:token').put(resetPassword)
 router.route('/me').get(isAuthenticatedUser, getUserProfile)
+router.route('/me/update').put(isAuthenticatedUser, updateProfile)
 router.route('/password/update').put(isAuthenticatedUser, updatePassword)
 
 router.route('/logout').get(logout)
