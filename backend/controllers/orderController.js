c3ff8f0e8116eb30f475e6c0c0996d66b79b6b42
razamehdisyed <razamehdisyed@gmail.com>
2023-05-31 10:05:27 +0500

    order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -35,3 +35,41 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) =>  {
         order
     })
 })
+// Get single order => api/v1/order/:id
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate('user','name email')
+
+    if (!order) {
+        return next(new ErrorHandler('Order not found with this ID', 404))
+    }
+    res.status(200).json ({
+        success: true,
+        order
+    })
+})
+// Get logged in user order => api/v1/order/me
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find({user: req.user.id})
+
+    
+    res.status(200).json ({
+        success: true,
+        orders
+    })
+})
+
+// Get all order by admin => api/v1/admin/orders
+exports.allOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find()
+
+    let totalAmount = 0
+    orders.forEach(order => {
+        totalAmount += order.totalPrice
+    })
+
+    res.status(200).json ({
+        success: true,
+        totalAmount,
+        orders
+    })
+})
